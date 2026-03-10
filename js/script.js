6c829853c7fdc770fd94cd3515bacc5279459f1a
sergio <sergioandrespuentescicery@gmail.com>
2026-03-09 19:08:12 -0500

    Configurando los productos en venta y añadiendoles imagenes representativas

--- js/script.js
+++ js/script.js
@@ -33,29 +33,30 @@ function enviarFormulario() {
     let mensaje = document.getElementById("mensaje").value
 
 
-
-    if (nombre == "" || nombre.test(//)) {
-        alert("Debes llenar el campo de nombre")
+    if (nombre == "" || nombre.test("\w+$") == false) {
+        alert("Datos incorrectos, por favor ingresa un nombre válido")
     }
-        else if (apellido == ""|| apellido.test(//)) {
-            alert("Debes llenar el campo de apellido")
+        else if (apellido == ""|| apellido.test(/^\w$/) == false) {
+            alert("Datos incorrectos, por favor ingresa un apellido válido")
         }
-        else if (correo == "" || correo.test(//)) {
-            alert("Debes llenar el campo de correo")
+        else if (correo == "" || correo.test(/^(a-zA-z0-9)+@([a-z])+\.com$/) == false) {
+            alert("Datos incorrectos, por favor ingresa un correo válido")
         }
-        else if (con_correo == "" || con_correo.test(//)) {
-            alert("La confirmación de correo es obligatoria")
+        else if (con_correo == "" || con_correo.test(/^(a-zA-z0-9)+@([a-z])+\.com$/) == false || con_correo != correo) {
+            alert("Datos faltates o incorrectos, por favor confirma tu correo")
         }
-        else if (edad == "" || edad.test(//)) {
+        else if (edad == "" || edad.test(/\d/ig) == false) {
             alert("Debes llenar el campo de edad")
         }
-        else if (direccion == "" || direccion.test(//)) {
+        else if (direccion == "") {
             alert("Debes llenar el campo de dirección")
         }
-        else if (telefono == "" || telefono.test(//)) {
+        else if (telefono == "" || telefono.test(/^3\d{9}$/) == false) {
             alert("Debes llenar el campo de teléfono")
         }
 
+
+    alert("Datos diligenciados:\nNombre: " + nombre + "\nApellido: " + apellido + "\nCorreo: " + correo + "\nEdad: " + edad + "\nGénero: " + genero + "\nDirección: " + direccion + "\nTeléfono: " + telefono + "\nMensaje: " + mensaje)
     console.log("Formulario enviado")
 
 }
